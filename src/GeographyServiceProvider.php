32d44a79294e7495bf99e0fb69d4601603aa4c9a
Brandon Best <brandonbest@dreamideation.com>
2018-07-30 23:27:34 -0700

    Update name of package

--- src/GeographyServiceProvider.php
+++ src/GeographyServiceProvider.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace Geography;
-
 /**
  * This file is part of Laratrust,
  * a role & permission management solution for Laravel.
@@ -9,12 +7,11 @@ namespace Geography;
  * @license MIT
  * @package Laratrust
  */
-
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\ServiceProvider;
-
 class GeographyServiceProvider extends ServiceProvider
 {
+
     /**
      * Indicates if loading of the provider is deferred.
      *
@@ -43,6 +40,53 @@ class GeographyServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // Register published configuration.
+        $this->publishes([
+            __DIR__.'/config/geography/us.php' => config_path('geography/us.php'),
+            __DIR__.'/config/geography/ca.php' => config_path('geography/ca.php'),
+        ], 'geography');
     }
 
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerGeography();
+        $this->mergeConfig();
+    }
+
+
+    /**
+     * Register the application bindings.
+     *
+     * @return void
+     */
+    private function registerLaratrust()
+    {
+        $this->app->bind('geography', function ($app) {
+            return new Geography\Geography($app);
+        });
+        $this->app->alias('geography', 'Geography\Geography');
+    }
+
+
+    /**
+     * Merges user's and laratrust's configs.
+     *
+     * @return void
+     */
+    private function mergeConfig()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/config/geography/us.php',
+            'geography'
+        );
+        $this->mergeConfigFrom(
+            __DIR__.'/config/geography/ca.php',
+            'geography'
+        );
+    }
 }
